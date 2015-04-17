9453d4b889e347ae1dfe72e14a793669126c8de1
Tom Haycock-West <tomhaycockwest@gmail.com>
2015-04-17 11:08:20 +0100

    fix movement on nav scroll

--- build/js/app.js
+++ build/js/app.js
@@ -3,35 +3,38 @@
 // If using JSLint for syntax debugging, include the following
 /*global $, console, alert, App*/
 
-var imageIndex = 0;
-var imagesArray = [
-    "home-bmw.jpg",
-    "home-merc.jpg",
-    "home-car.jpg"
-];
+// var imageIndex = 0;
+// var imagesArray = [
+//     "home-bmw.jpg",
+//     "home-merc.jpg",
+//     "home-car.jpg"
+// ];
 
-function changeBackground(){
-    var index = imageIndex++ % imagesArray.length;
-    // $("#intro").css("background","url('assets/img/"+imagesArray[index] +"')50% 0 fixed");      
+// function changeBackground(){
+//     var index = imageIndex++ % imagesArray.length;
+//     // $("#intro").css("background","url('assets/img/"+imagesArray[index] +"')50% 0 fixed");      
 
 
-	$('.slider').fadeTo('slow', 0, function()
-	{
-	    $(this).css("background","url('assets/img/"+imagesArray[index] +"')50% 0 no-repeat");
-	}).fadeTo('slow', 1);
-}
+// 	$('.slider').fadeTo('slow', 0, function()
+// 	{
+// 	    $(this).css("background","url('assets/img/"+imagesArray[index] +"')50% 0 no-repeat");
+// 	}).fadeTo('slow', 1);
+// }
 
 $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = 10;
 
     if(y_scroll_pos > scroll_pos_test) {
-        $('.navbar-brand > img').addClass('small', 'col-md-1');
-        $('.nav > li:first').removeClass('col-md-2');
+        $('.navbar-brand > img').attr("src","assets/img/nav-logo-small.png");
+        // $('.nav > li:first').removeClass('col-xs-2');
+
+        $('.first-nav').removeClass('margin');
         $('.navbar').addClass('black');
     } else {
-        $('.navbar-brand > img').removeClass('small', 'col-md-1');
-        $('.nav > li:first').addClass('col-md-2');
+        $('.navbar-brand > img').attr("src","assets/img/nav-logo.png");
+        // $('.nav > li:first').addClass('col-xs-2');
+        $('.first-nav').addClass('margin');
         $('.navbar').removeClass('black');
     }
 });
@@ -63,7 +66,7 @@ var App = {
 		this.listen();
 		// Application has been initalized
 		console.log(this.settings.name + "(v" + this.settings.version + ") Started");	
-		setInterval(changeBackground, 3000);
+		// setInterval(changeBackground, 3000);
 
 	}
 
